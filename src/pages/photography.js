58168f1243b83b6fc4d6ed8899f4e8871a686e54
Chris Collareta <ccollareta@gmail.com>
2023-07-11 15:57:36 -0400

    Update photography.js

--- src/pages/photography.js
+++ src/pages/photography.js
@@ -75,7 +75,7 @@ const PhotoPage = ({ data }) => {
 export const pageQuery = graphql`
   query {
     allMarkdownRemark(
-      filter: { fields: { sourceName: { eq: "portfolio" } } }
+      filter: { fields: { sourceName: { eq: "photography" } } }
       sort: { fields: frontmatter___date, order: DESC }
     ) {
       edges {
